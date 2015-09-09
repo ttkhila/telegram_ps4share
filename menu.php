1d66a1a89656c829060feef6265e7ec952d9a88a
ttkhila <e_rocha78@yahoo.com.br>
2015-09-09 15:04:18 -0300

    menu

--- menu.php
+++ menu.php
@@ -1,16 +1,19 @@
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="true" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">Telegram - Partilhas</a>
+          <a class="navbar-brand" href="#"><span class="glyphicon glyphicon-share"></span> Telegram - Partilhas</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</li>
+          </ul>
+          <ul class="nav navbar-nav navbar-left">
             <li><a href="./">Home</a></li>
             <li><a href="grupos.php">Gerenciar Grupos</a></li>
             <li><a href="jogos.php">Gerenciar Jogos</a></li>
@@ -19,10 +22,12 @@
             <li><a href="#">Perfil</a></li>
             <li style='margin-right:70px;'>%%user%%</li>
           </ul>
+          <!--
           <form class="navbar-form navbar-right">
             <input type="text" class="form-control" placeholder="Pesquisar no site...">
             <button type="submit" class="btn btn-default">Buscar</button>
           </form>
+          -->
      	</div>
       </div>
     </nav>
